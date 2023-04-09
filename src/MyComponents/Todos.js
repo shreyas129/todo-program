44b8dd586ed9541ebecbe9e84baa05201486ac32
shreyas129 <shreyas.tathode129@gmail.com>
2023-04-09 23:16:37 +0530

    added line in the bottom of Todos.js

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -7,7 +7,11 @@ export const Todos = (props) => {
             <h3 className='my-3'>Todos List</h3>
             {props.todos.length === 0 ? "No Todos to display" :
                 props.todos.map((todo) => {
-                    return <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} />
+                    return (
+                        <>
+                            <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete} /> <hr />
+                        </>
+                    )
                 })
             }
         </div>
