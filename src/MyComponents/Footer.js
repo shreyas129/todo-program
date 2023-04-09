e0018642a332fb32cb31c188127228435a4b80f7
shreyas129 <shreyas.tathode129@gmail.com>
2023-04-09 22:44:52 +0530

    Initial commit

---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Footer = () => {
+    let footerStyle = {
+        position: "relative",
+        top: "100%",
+        width: "100%",
+        border: "1.0000000001px solid red"
+       }
+    return (
+        <footer className='bg-dark text-light py-3' style={footerStyle}>
+            <p className="text-center">
+                Copyright &copy; shreyasTathode.com
+            </p>
+        </footer>
+    )
+}
